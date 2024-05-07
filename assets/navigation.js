0e5773239b17aa97cf038f6b30254c019d5aaca2
BelfordZ <semantic-release-bot@martynus.net>
2024-05-07 18:59:29 +0000

    github pages release

---
+++ assets/navigation.js
@@ -0,0 +1,1 @@
+window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAAA23OQQrCMBAF0LvMOhittmIP4E7cuBMXYzuhwZiWZFIU6d2lIK2JrmYx/z/++QVMD4YSTg57cp6OHevWehDQITdQgrZMTmFFXiaZRcN3AwJu2tZQZnkxiIk7BMYxtA+2Gu/s8bMjL9N/Yi1321WefXk1KQyGf9b16DReDXkZJ2Jv/YeaDfXZMBlxudgMlzcmT/7QKAEAAA=="
